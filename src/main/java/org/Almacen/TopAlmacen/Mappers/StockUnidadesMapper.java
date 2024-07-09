22ada8126369d5fba36353525ee24c2fcfc4457e
Gerson Henriquez Avila <u21206891@utp.edu.pe>
2024-07-09 00:05:19 -0500

    Concat completo

--- src/main/java/org/Almacen/TopAlmacen/Mappers/StockUnidadesMapper.java
+++ src/main/java/org/Almacen/TopAlmacen/Mappers/StockUnidadesMapper.java
@@ -2,7 +2,11 @@ package org.Almacen.TopAlmacen.Mappers;
 
 import org.Almacen.TopAlmacen.DTO.StockUnidades.StockUnidadesDto;
 import org.Almacen.TopAlmacen.DTO.StockUnidades.UpdateStockUnidadesDto;
+import org.Almacen.TopAlmacen.Model.PrecioPorTipoUnidad;
 import org.Almacen.TopAlmacen.Model.StockUnidades;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StockUnidadesMapper {
     public static StockUnidadesDto toStockUnidadesDto(StockUnidades stockUnidades) {
@@ -13,5 +17,14 @@ public class StockUnidadesMapper {
         StockUnidades s = new StockUnidades();
         s.setCantidadStockUnidad(dto.getCantidadStockUnidadesDto());
         return s;
+    }
+
+    public static String concatFromList(List<StockUnidades> list) {
+        if (list == null || list.isEmpty()) {
+            return "";
+        }
+        var fistStock = list.get(0);
+        var getProduct = fistStock.getPrecios().get(0).getProducto();
+        return ProductoMapper.toConcatProduct(getProduct);
     }
 }
