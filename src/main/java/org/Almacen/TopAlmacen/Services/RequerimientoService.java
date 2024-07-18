bd12de078a79848024caf473567b3c7e72127032
Gerson Henriquez Avila <u21206891@utp.edu.pe>
2024-07-17 22:23:57 -0500

    Transacional terminado

--- src/main/java/org/Almacen/TopAlmacen/Services/RequerimientoService.java
+++ src/main/java/org/Almacen/TopAlmacen/Services/RequerimientoService.java
@@ -4,6 +4,7 @@ package org.Almacen.TopAlmacen.Services;
 import jakarta.ejb.LocalBean;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import org.Almacen.TopAlmacen.DAO.IItemsRequerimientoDao;
 import org.Almacen.TopAlmacen.DAO.IRequerimientoDao;
 import org.Almacen.TopAlmacen.DTO.ItemsRequerimiento.CreateItemsRequerimientoDto;
@@ -27,19 +28,22 @@ public class RequerimientoService {
     @Inject
     private IItemsRequerimientoDao iItemsRequerimientoDao;
 
+    @Transactional
     public List<Requerimiento> getRequerimientos() {
         return iRequerimientoDao.getAll();
     }
 
+    @Transactional
     public List<Requerimiento> getRequerimientosbyDependencia(int idDependencia) {
         return iRequerimientoDao.getRequerimientoByDependencia(idDependencia);
     }
 
-
+    @Transactional
     public Requerimiento getRequerimiento(String id) {
         return iRequerimientoDao.getById(id);
     }
 
+    @Transactional
     public Requerimiento create(CreateRequerimientoDto dto, List<CreateItemsRequerimientoDto> itemsDto) {
         var created = RequerimientoMapper.fromCreate(dto);
         iRequerimientoDao.create(created);
@@ -51,23 +55,28 @@ public class RequerimientoService {
         return created;
     }
 
+    @Transactional
     public Requerimiento update(UpdateRequerimientoDto dto, String id) {
         return iRequerimientoDao.update(dto, id);
     }
 
+    @Transactional
     public Requerimiento delete(String id) {
         iItemsRequerimientoDao.deleteRequerimientoAll(id);
         return iRequerimientoDao.delete(id);
     }
 
+    @Transactional
     public List<Requerimiento> getAllAprobed() {
         return iRequerimientoDao.getAllAprobed();
     }
 
+    @Transactional
     public void setEstadoAprobado(String id, String observacion) {
         iRequerimientoDao.setEstado(id, "APROBADO", observacion);
     }
 
+    @Transactional
     public void setEstadoDesaprobado(String id, String observacion) {
         iRequerimientoDao.setEstado(id, "DESAPROBADO", observacion);
     }
